0ffc2f98167b53c25231a5bf674e81cc7e1d073c
chrono234 <jairoor01outlook.com>
2023-06-04 10:50:56 -0600

    base updated

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from "path";
 
-const aliases = {
-  '@crema': 'src/@crema',
-  'core': 'src/core',
-  'assets': 'src/assets',
-  '@hook': 'src/@hook',
-  'components': 'src/components',
-  'features': 'src/features',
-  'guards': 'src/guards',
-  'pages': 'src/pages',
-  'types': 'src/types',
-};
 
-const resolvedAliases = Object.fromEntries(
-  Object.entries(aliases).map(([key, value]) => [key, resolve('https://chrono234.github.io/funcionalidad-twitter/', value)]),
-);
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-      rollupOptions: {
-          external: [
-              "react", // ignore react stuff
-              "react-dom",
-          ],
-      }
-  },
-
-  resolve: {
-    alias: {
-        ...resolvedAliases,
-        './runtimeConfig': './runtimeConfig.browser',
-        'jss-plugin-{}': 'jss-plugin-global'
-    },
-},
+  base:'/funcionalidad-twitter/',
 })
